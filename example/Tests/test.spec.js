f77854a692887690554176a6ccd5d912af2f83de
marina51533 <marina51533@gmail.com>
2024-04-29 15:23:29 +0200

    Update4

--- example/Tests/test.spec.js
+++ example/Tests/test.spec.js
@@ -11,5 +11,7 @@ describe("Fer share Test suite", () => {
      `//*[@resource-id="com.FerShare.fershare:id/editTextFuelConsume"]`
    );
    await averageCons.setValue("2");
+
+   console.log(averageCons)
   });
 });
